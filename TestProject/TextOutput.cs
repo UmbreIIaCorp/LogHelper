a384f799d98a483356dd728fe41c9114970dd94f
hp <ruslan.zubashenko@gmail.com>
2017-04-08 22:30:02 +0300

    1

---
+++ TestProject/TextOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject {
+    public class TextOutput : TextWriter {
+        private List<Tuple<TextWriter>> _writers;
+        public TextOutput() {
+            _writers = new List<Tuple<TextWriter>>();
+        }
+
+
+        public override Encoding Encoding {
+            get {
+                return Encoding.UTF8;
+            }
+        }
+
+        public new void  Write (string message) {
+            for (int i = 0; i < _writers.Count; i++) {
+                _writers[i].Item1.WriteLine(message);
+            }
+        }
+
+        public void RegisterWriter(TextWriter writer) {
+            this._writers.Add(new Tuple<TextWriter>(writer));
+        }
+    }
+}
